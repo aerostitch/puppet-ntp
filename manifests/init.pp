0825267ec1f0e39ee54af3ae237fcb680ec73de2
lofic <louis.coilliot@gmail.com>
2013-03-06 16:57:38 +0100

    Massive reimport as an individual module

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+class ntp {
+    package { 'ntp' :
+        ensure  => installed,
+    }
+
+    $ntpservice = $::osfamily ? {
+        'Debian' => 'ntp',
+        default  => 'ntpd',
+    }
+
+    service { $ntpservice :
+        ensure  => running,
+        require => Package[ 'ntp' ],
+        enable  => true,
+    }
+}
+
