a385d6fcb325eac758ddf1186c78eb9c62ee9b4a
Joseph HERLANT <joseph.herlant@vallourec.fr>
2013-08-19 09:02:44 +0200

    Adding support for AIX Adding template files for ntp configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,54 @@
 # Time synchronization
 
 class ntp {
-    package { 'ntp' :
-        ensure  => installed,
+    $rootgrp = $::operatingsystem ? {
+        'AIX'   => 'system',
+        default => 'root',
+    }
+    
+    $ntpfile = $::operatingsystem ? {
+        'AIX'   => 'aix-ntp.conf',
+        default => 'lnx-ntp.conf',
     }
 
     $ntpservice = $::osfamily ? {
         'Debian' => 'ntp',
+        'AIX' => 'xntpd',
         default  => 'ntpd',
     }
 
-    service { $ntpservice :
-        ensure  => running,
-        require => Package[ 'ntp' ],
-        enable  => true,
+
+    file { 'ntp.conf' :
+        ensure  => 'file',
+        path    => '/etc/ntp.conf',
+        source  => "puppet:///modules/ntp/${ntpfile}",
+        owner   => 'root',
+        group   => $rootgrp,
+        mode    => '0640',
+        #require => Package[ 'ntp' ],
+    }
+
+    if $::osfamily == 'RedHat' {
+        package { 'ntp' :
+            ensure  => installed,
+        }
+
+        service { $ntpservice :
+            ensure  => running,
+            require => [ Package[ 'ntp' ], File[ 'ntp.conf' ], ],
+            enable  => true,
+            subscribe => File[ 'ntp.conf' ],
+        }
+    }elsif $::osfamily == 'AIX' {
+        package { 'bos.net.tcp.client' :
+            ensure  => installed,
+        }
+
+       service { $ntpservice :
+            ensure  => running,
+            require => [ Package[ 'bos.net.tcp.client' ], File[ 'ntp.conf' ], ],
+            enable  => true,
+            subscribe => File[ 'ntp.conf' ],
+        } 
     }
 }
-
